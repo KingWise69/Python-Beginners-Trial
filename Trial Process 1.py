45c970b13350fcaeb8b217550968571ac8dfceae
Wise <101383274+KingWise69@users.noreply.github.com>
2022-10-03 20:02:46 +0300

    Add files via upload
    THE PYTHON JOURNEY BEGINS

---
+++ Trial Process 1.py
@@ -0,0 +1,52 @@
+# age = 21
+# print(age)
+# print("My Name is Magezi Joshua Wisdom\nMy age is 'age'")
+# price = 45.7
+# first_name = 'Magezi'
+# second_name = 'Joshua'
+# third_name = 'Wisdom'
+# is_online = False
+
+#Receiving Input From A User
+
+# name = input("What is your Name?\n")
+# print("Welcome " + name)
+
+#3 TYPES OF DATA
+# Strings = "WISE"
+# Numbers = 100
+# Booleans = TRUE OR FALSE
+
+# year_of_birth = input("Enter Your Year of Birth: ")
+
+# age = 2022 - int(year_of_birth)
+# print (age)
+# Floating Point Number - These are numbers with a decimal piont
+# The Built in function for converting Variables
+# 1. int() - to an integer
+# 2. float() - to a decimal number
+# 3. bool() - converts numbers to boolean
+# 4. str() - converts numbers to strings
+
+
+# number_pizzas_ordered = input("Enter The Number of Pizzas order for: ")
+# number = 5000 * int(number_pizzas_ordered)
+# print(number)
+
+# first_number = input("Enter First Number: ")
+# second_number = input("Enter Second Number: ")
+# sum = float(first_number) + float(second_number)
+# print('Sum:' + str(sum))
+
+# first_number = float(input("Enter First Number: "))
+# second_number = float(input("Enter Second Number: "))
+# third_number = float(input("Enter Third Number: "))
+
+# sum = first_number + second_number + third_number
+# print(sum)
+
+romeo ='misissipi queen'
+print(romeo.upper())
+print(romeo.find('s'))
+print(romeo.replace('queen', 'island'))
+print('Jack' in romeo)
